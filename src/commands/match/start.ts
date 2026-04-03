5bb28e60382198d0a2e16f877f77d4d8788d85c8
primepvi <pedrobernardesv@gmail.com>
2026-04-02 22:11:53 -0300

    feat: implementing match db registration.

--- src/commands/match/start.ts
+++ src/commands/match/start.ts
@@ -1,12 +1,26 @@
 import { ChatInputCommandInteraction } from "discord.js";
 import { createSubCommand } from "../../factory/command";
+import { db } from "../../schemas";
 
 export default createSubCommand({
     parent: "match",
     name: "start",
     description: "Utilize este comando para iniciar uma partida.",
     options: [],
-    execute(interaction: ChatInputCommandInteraction) {
-      interaction.reply("foi");
+    async execute(interaction: ChatInputCommandInteraction) {
+      const userData = (await db.users.findById(interaction.user.id))!;
+      if (userData.match) {
+	interaction.reply("Você já tem uma partida em andamento.");
+	return;
+      }
+
+      const match = await db.matches.create({
+	userId: interaction.user.id
+      });
+
+      userData.match = match._id;
+      await userData.save();
+
+      interaction.reply("Você iniciou uma partida com sucesso.");
     },
 });
